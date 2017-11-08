88a7e216e882b1fab6abafb40d3f02ec36fcd768
Thomas Eckert <thomas.eckert@rochester.edu>
2017-11-08 18:18:50 -0500

    +dirs, +solns, iss. 7.1

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Thomas Eckert on 11/7/17.
+//  Copyright © 2017 Thomas Eckert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
