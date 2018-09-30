30d022309c62c3a548df745ecba3daa89e5477f1
t-eckert <t_eckert@outlook.com>
2018-09-30 12:38:13 -0700

    + test coverage

--- 01-Arrays and Strings/test_q4.py
+++ 01-Arrays and Strings/test_q4.py
@@ -2,10 +2,14 @@ import unittest
 import q4_Is_Palindrome
 
 class TestIsPalindrome(unittest.TestCase):
-    def test_check_palindrome_permute_with_false_string(self):
+    def test_check_palindrome_permute_with_False_string(self):
         test_value = "bananagram"
         self.assertFalse(q4_Is_Palindrome.check_palindrome_permute(test_value))
 
     def test_check_palindrome_permute_with_True_string(self):
         test_value = "Tact coa"
         self.assertTrue(q4_Is_Palindrome.check_palindrome_permute(test_value))
+
+    def test_check_palindrome_permute_with_odd_numbered_False_string(self):
+        test_value = "languid"
+        self.assertFalse(q4_Is_Palindrome.check_palindrome_permute(test_value))
